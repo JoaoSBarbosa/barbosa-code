6e85b61eb5566ce988d999f63f638cf9d99e6a1a
João Barbosa <contato.jsbarbosa@gmail.com>
2024-01-11 22:41:25 -0300

    Personalização da imagem principal

--- src/app/components/main/Introduction.tsx
+++ src/app/components/main/Introduction.tsx
@@ -7,45 +7,32 @@ import {colorOptions, NavButton} from "@/app/components/utils/buttons/NavButton"
 import {useEffect, useState} from "react";
 
 export const Introduction = () => {
-    const [isTyping, setIsTyping] = useState(false);
     const [title, setTitle] = useState('');
-    const [showCursor, setShowCursor] = useState(true);
 
-    useEffect(() => {
-        // Ativar a máquina de escrever após o componente ser montado
-        setIsTyping(true);
-    }, []);
 
-    useEffect(() => {
-        const titleText = 'Olá, eu sou João Barbosa';
-        let index = 0;
 
-        const intervalId = setInterval(() => {
-            if (index <= titleText.length) {
-                // Exibir as letras gradualmente
-                setTitle(titleText.slice(0, index));
-                index += 1;
-            } else {
-                // Alternar o estado para mostrar/ocultar o cursor piscante
-                setShowCursor((prev) => !prev);
-            }
-        }, 100); // Ajuste o valor do intervalo conforme necessário
-    }, []);
+    // useEffect(() => {
+    //     const titleText = 'Olá, eu sou João Barbosa';
+    //     let index = 0;
+    //     const intervalId = setInterval(() => {
+    //         if (index <= titleText.length) {
+    //             setTitle(titleText.slice(0, index));
+    //             index += 1;
+    //         }
+    //     }, 110);
+    //     return () => clearInterval(intervalId);
+    // }, []);
 
     return (
         <main className={"introduction-container bg-black"}>
 
             <div className={`${styles.introduction}  text-white max-w-screen-xl mx-auto`}>
                 <div className={`${styles.introductionContent}`}>
-                    {/*<h1 className={"text-4xl font-bold"}>*/}
-                    {/*    {isTyping && 'Olá, eu sou João Barbosa'}*/}
-                    {/*    <span className={"text-customPurple"}>.</span>*/}
-                    {/*</h1>*/}
                     <h1 className={"text-4xl font-bold"}>
-                        {title}
-                        {isTyping && showCursor && <span className={"text-customPurple"}>.</span>}
+                        Olá, eu sou João Barbosa
+                        <span className={"text-customPurple rounded"}>.</span>
                     </h1>
-                    <p>
+                    <p className={""}>
                         Desenvolvedor Full Stack Júnior apaixonado por criar soluções web inovadoras.
                         Minhas habilidades abrangem o uso de tecnologias como Java, Spring, SQL,
                         JavaScript e React. Estou comprometido em oferecer resultados excepcionais
@@ -62,7 +49,7 @@ export const Introduction = () => {
                 </div>
                 <div className={`${styles.introductionImage}`}>
                     <img
-                        src={"/img/jb.png"}
+                        src={"/img/barbosa.png"}
                         width={500}
                         className={`${styles.image}`}
                         alt={"João Barbosa, desenvolvedor Full Stack Júnior, sorrindo com óculos de grau e camiseta social."}/>
