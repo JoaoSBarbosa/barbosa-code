3d45cc318dd17c4715194fa6432315ebb2b385bd
João Barbosa <contato.jsbarbosa@gmail.com>
2024-05-21 20:18:15 -0300

    Correção de responsividade na página about

--- src/app/components/about/profileBanner/index.tsx
+++ src/app/components/about/profileBanner/index.tsx
@@ -1,57 +1,73 @@
-import React, {useState} from 'react';
-import {LayoutAbout} from "../layoutAbout";
-import {FaGithub, FaInstagram, FaLinkedin} from "react-icons/fa";
-import styles from './ProfileBanner.module.css';
+import React, { useState } from "react";
+import { LayoutAbout } from "../layoutAbout";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import styles from "./ProfileBanner.module.css";
 
 interface Social {
-    id: number;
-    title: string;
-    href: string;
-    target: string;
-    icon: React.ElementType;
+  id: number;
+  title: string;
+  href: string;
+  target: string;
+  icon: React.ElementType;
 }
 
 const socials: Social[] = [
-    {id: 1, title: "Github", href: "https://github.com/JoaoSBarbosa", target: "_blank", icon: FaGithub},
-    {id: 2, title: "Linkedin", href: "https://www.linkedin.com/in/devjbarbosa/", target: "_blank", icon: FaLinkedin},
-    {id: 3, title: "Instagram", href: "https://instagram.com/jsbarbosadev", target: "_blank", icon: FaInstagram},
+  {
+    id: 1,
+    title: "Github",
+    href: "https://github.com/JoaoSBarbosa",
+    target: "_blank",
+    icon: FaGithub,
+  },
+  {
+    id: 2,
+    title: "Linkedin",
+    href: "https://www.linkedin.com/in/devjbarbosa/",
+    target: "_blank",
+    icon: FaLinkedin,
+  },
+  {
+    id: 3,
+    title: "Instagram",
+    href: "https://instagram.com/jsbarbosadev",
+    target: "_blank",
+    icon: FaInstagram,
+  },
 ];
 
 export const ProfileBanner: React.FC = () => {
-    const [hoveredIcon, setHoveredIcon] = useState<number | null>(null);
+  const [hoveredIcon, setHoveredIcon] = useState<number | null>(null);
 
-    return (
-
-
+  return (
     <LayoutAbout title={"Redes Sociais"}>
-
-        <div className={`${styles.container}`}>
-            <div className={" flex flex-col gap-10 top-5"}>
-                {socials.map((social) => (
-                    <div className={"flex items-center"} key={social.id}>
-                        <a
-                            href={social.href}
-                            target={social.target}
-                            className={"mr-4 p-2"}
-                            onMouseEnter={() => setHoveredIcon(social.id)}
-                            onMouseLeave={() => setHoveredIcon(null)}
-                        >
-                            {React.createElement(social.icon, {
-                                size: 35,
-                                className: "cursor-pointer",
-                                title: social.title
-                            })}
-                        </a>
-                        {hoveredIcon === social.id && <span
-                            className={"transition bg-white text-black rounded-r-lg p-2"}>{social.title} - {social.href}</span>}
-                    </div>
-                ))}
+      <div className={`${styles.container}`}>
+        <div className={" flex flex-col gap-10 top-5"}>
+          {socials.map((social) => (
+            <div className={"flex items-center"} key={social.id}>
+              <a
+                href={social.href}
+                target={social.target}
+                className={"mr-4 p-2"}
+                onMouseEnter={() => setHoveredIcon(social.id)}
+                onMouseLeave={() => setHoveredIcon(null)}
+              >
+                {React.createElement(social.icon, {
+                  size: 35,
+                  className: "cursor-pointer",
+                  title: social.title,
+                })}
+              </a>
+              {hoveredIcon === social.id && (
+                <span
+                  className={"transition bg-white text-black rounded-r-lg p-2"}
+                >
+                  {social.title} - {social.href}
+                </span>
+              )}
             </div>
+          ))}
         </div>
-
+      </div>
     </LayoutAbout>
-
-
-
-    );
-}
+  );
+};
--- src/app/components/about/profileHeader/index.tsx
+++ src/app/components/about/profileHeader/index.tsx
@@ -1,47 +1,45 @@
-import {LayoutAbout} from "../layoutAbout";
-import {TechnologiesData} from "@/app/list/technologies/technologiesData";
+import { LayoutAbout } from "../layoutAbout";
+import { TechnologiesData } from "@/app/list/technologies/technologiesData";
 import styles from "./ProfileHeader.module.css";
 
 export const ProfileHeader = () => {
-    return (
-        <LayoutAbout>
+  return (
+    <LayoutAbout>
+      <div className={`flex gap-10 flex-col md:flex-row `}>
+        <div className={"flex  items-center justify-center  md:w-3/12"}>
+          <figure
+            className={
+              "bg-purple-600  flex items-center w-full max-w-96 justify-center rounded-full overflow-hidden"
+            }
+          >
+            <img
+              className={`block ${styles.image}`}
+              src="../img/barbosa.gif"
+              alt="Foto do perfil"
+            />
+          </figure>
+        </div>
 
-            <div className={`flex gap-10`}>
-
-                <div className={" flex items-center justify-center w-3/12"}>
-                    <figure
-                        className={"bg-purple-600  flex items-center w-full justify-center rounded-full overflow-hidden"}>
-                        <img
-                            className={`block ${styles.image}`}
-                            src="../img/barbosa.gif"
-                            alt="Foto do perfil"
-                        />
-                    </figure>
-
-                </div>
-                <div className={"mx-1 w-9/12 flex flex-col gap-10 items-start"}>
-                    <h2 className={"text-5xl"}>Olá ! Sou o João</h2>
-                    <p className={"text-2xl break-words mt-10"}>
-                        Desenvolvedor fullstack utilizando as linguagens Java, Javascript e blibiotecas e framework
-                        Spring e Nextjs, para o desenvolvimento
-                        e manutenção de páginas web, sistemas e componentes de interface em WordPress. Atuando em
-                        projetos de e-commerce.
-                    </p>
-                    <div className="flex  box-border flex-wrap gap-10 lg:gap-5 w-full items-center justify-start">
-                        {TechnologiesData.map((tech, index) => (
-                            <img
-                                key={index}
-                                src={tech.uraImage}
-                                alt={tech.technology}
-                                className="w-8"
-                            />
-                        ))}
-                    </div>
-
-                </div>
-            </div>
-
-        </LayoutAbout>
-
-    )
-}
+        <div className={"mx-1 md:w-9/12 flex flex-col gap-10 items-start"}>
+          <h2 className={"text-5xl"}>Olá ! Sou o João</h2>
+          <p className={"text-2xl break-words mt-10"}>
+            Desenvolvedor fullstack utilizando as linguagens Java, Javascript e
+            blibiotecas e framework Spring e Nextjs, para o desenvolvimento e
+            manutenção de páginas web, sistemas e componentes de interface em
+            WordPress. Atuando em projetos de e-commerce.
+          </p>
+          <div className="flex  box-border flex-wrap gap-10 lg:gap-5 w-full items-center justify-start">
+            {TechnologiesData.map((tech, index) => (
+              <img
+                key={index}
+                src={tech.uraImage}
+                alt={tech.technology}
+                className="w-8"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </LayoutAbout>
+  );
+};
